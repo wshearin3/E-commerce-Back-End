4b8ae66fea2251423f44e65392344cb863a2d2b1
Trey <trey.shearin3@gmail.com>
2024-05-02 22:04:04 -0400

    created get route for category id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -15,7 +15,22 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      // JOIN with travellers, using the Trip through table
+      include: [{ model: Product }]
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find one category by its `id` value
   // be sure to include its associated Products
 });
