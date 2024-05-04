1b6c1becb864094bcfe918311dbbcdcfa9dfb9cc
Trey <trey.shearin3@gmail.com>
2024-05-04 17:03:12 -0400

    updated model information for Product and Product Tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,9 +3,18 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagData = await Category.findAll({
+      // be sure to include its associated Products
+    include: [{ model: Product }]})
+    res.status(200).json (categoryData)
+  }
+  catch(err){
+    res.status(500).json (err)
+  }
 });
 
 router.get('/:id', (req, res) => {
